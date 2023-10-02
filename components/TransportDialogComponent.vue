0b01cd5cc72d673d3ef988aad7f979ef03bc5a2e
fwesterath <fwesterath@ethz.ch>
2023-10-02 16:42:39 +0200

    started transport dialog compnent

--- components/TransportDialogComponent.vue
+++ components/TransportDialogComponent.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { useNotificationsStore } from 'store/notifications';
+import { NotificationType } from 'types/components/Notification';
 import { Transport } from '~/types/api/Transport';
 
 const props = defineProps({
@@ -9,6 +11,8 @@ const props = defineProps({
 })
 const propTransport = props.transport as Transport
 
+const notificationStore = useNotificationsStore();
+
 const transport = {
     deliveryDate: '',
     isExternal: false,
@@ -18,13 +22,44 @@ const transport = {
         id: 0
     }
 }
+
+if (propTransport) {
+    transport.deliveryDate = propTransport.deliveryDate as string
+    transport.isExternal = propTransport.isExternal as boolean
+    transport.isApproved = propTransport.isApproved as boolean
+    transport.isCompleted = propTransport.isCompleted as boolean
+    transport.project.id = propTransport.project.id as number
+}
+
+const dialog = ref<HTMLDialogElement | null>(null)
+
+const openDialog = () => {
+    dialog.value?.showModal()
+}
+
+const closeDialog = () => {
+    dialog.value?.close()
+}
+
+const createTransport = async () => {
+    try {
+        const createdTransport: Transport = await Transport.createTransport(Transport.fromJson(transport))
+        window.location.href = `/projects/${createdTransport.project.id}`
+    } catch (error : any) {
+        notificationStore.addNotificationToStore({
+            header: 'Error',
+            message: error.message,
+            type: NotificationType.ERROR,
+        })
+    }
+}
 </script>
 
 <template>
     <div class="create-transport">
 
 <div class="create-transport__open-button">
-    <button v-if="propProject" @click="openDialog" class="button--warning create-transport__open-button__button">
+    <button v-if="propTransport" @click="openDialog" class="button--warning create-transport__open-button__button">
         <font-awesome-icon :icon="['fas', 'pen']" />
     </button>
     <button v-else @click="openDialog" class="button--primary create-transport__open-button__button">
